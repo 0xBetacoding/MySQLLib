9f14c7464c371a4bd53799ae41442ac738f8e33f
Betacoding <betacoding@gmx.de>
2024-10-04 14:50:54 +0200

    Added MySQLConnectionProperty.TIMEOUT to configure specific connection establishment timeout

--- src/main/java/de/betacoding/mysql/MySQLConnectionBuilder.java
+++ src/main/java/de/betacoding/mysql/MySQLConnectionBuilder.java
@@ -5,7 +5,6 @@ import de.betacoding.util.DebugLogger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.net.InetSocketAddress;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +26,10 @@ public class MySQLConnectionBuilder {
         this.databaseName = databaseName;
         this.user = user;
         this.logger = DebugLogger.SYS_LOGGER;
+
+        for (MySQLConnectionProperty<?> property : MySQLConnectionProperty.defaultProperties()) {
+            this.properties.put(property, property.getDefaultValue());
+        }
     }
     public MySQLConnectionBuilder(@NotNull String protocol, @NotNull String host, int port, @NotNull String user) {
         this(protocol, host, port, "", user);
@@ -36,10 +39,6 @@ public class MySQLConnectionBuilder {
     }
     public MySQLConnectionBuilder(@NotNull MySQLURLProtocol protocol, @NotNull String host, int port, @NotNull String user) {
         this(protocol, host, port, "", user);
-    }
-    @Deprecated
-    public MySQLConnectionBuilder(@NotNull String protocol, @NotNull InetSocketAddress address, @NotNull String databaseName, @NotNull String user) {
-        this(protocol, address.getHostName(), address.getPort(), databaseName, user);
     }
 
     public @NotNull <T> MySQLConnectionBuilder setProperty(@NotNull MySQLConnectionProperty<T> property, @Nullable T value) {
--- src/main/java/de/betacoding/mysql/MySQLConnectionProperty.java
+++ src/main/java/de/betacoding/mysql/MySQLConnectionProperty.java
@@ -3,9 +3,24 @@ package de.betacoding.mysql;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+
 public class MySQLConnectionProperty<T> {
+    public static final MySQLConnectionProperty<Integer> TIMEOUT = new MySQLConnectionProperty<>("connect timeout", Integer.class, 10);
     public static final MySQLConnectionProperty<Boolean> AUTO_RECONNECT = new MySQLConnectionProperty<>("autoReconnect", Boolean.class, false);
     public static final MySQLConnectionProperty<String> CHARACTER_ENCODING = new MySQLConnectionProperty<>("characterEncoding", String.class, null);
+
+    private static final MySQLConnectionProperty<?>[] properties;
+    private static final MySQLConnectionProperty<?>[] defaultProperties;
+
+    static {
+        properties = new MySQLConnectionProperty[] {
+                TIMEOUT, AUTO_RECONNECT, CHARACTER_ENCODING
+        };
+        defaultProperties = Arrays.stream(properties)
+                .filter(property -> property.getDefaultValue() != null)
+                .toList().toArray(new MySQLConnectionProperty<?>[0]);
+    }
 
     private final String name;
     private final Class<T> type;
@@ -25,5 +40,12 @@ public class MySQLConnectionProperty<T> {
     }
     public @Nullable T getDefaultValue() {
         return this.defaultValue;
+    }
+
+    public static MySQLConnectionProperty<?>[] properties() {
+        return properties;
+    }
+    public static MySQLConnectionProperty<?>[] defaultProperties() {
+        return defaultProperties;
     }
 }
